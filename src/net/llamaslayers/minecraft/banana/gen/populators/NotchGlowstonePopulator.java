d7413bc8ed6371d471b5db06923ce81a080872fd
Ben L <nightgunner5@llamaslayers.net>
2011-07-26 10:35:02 -0500

    Cleanup, cleanup, everybody everywhere!

--- src/net/llamaslayers/minecraft/banana/gen/populators/NotchGlowstonePopulator.java
+++ src/net/llamaslayers/minecraft/banana/gen/populators/NotchGlowstonePopulator.java
@@ -6,7 +6,6 @@ import java.util.Random;
 
 // Bukkit Imports
 import org.bukkit.Chunk;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.BlockFace;
 
@@ -15,77 +14,73 @@ import net.llamaslayers.minecraft.banana.gen.BananaBlockPopulator;
 
 /**
  * Edited for BananaGen.
- * 
+ *
  * @author Markus 'Notch' Persson
  * @author iffa
  */
 public class NotchGlowstonePopulator extends BananaBlockPopulator {
-	// Variables
-	private Random rand = new Random();
-	private int x = 0;
-	private int y = 0;
-	private int z = 0;
-
 	/**
 	 * Populates a world with glowstone. Easily configurable (but results in
 	 * more rare glowstone) by modifying the suitable()-method.
-	 * 
+	 *
 	 * @author Markus 'Notch' Persson
 	 * @author iffa
 	 */
 	@Override
 	public void populate(World world, Random random, Chunk source) {
-		x = rand.nextInt(15);
-		y = rand.nextInt(127);
-		z = rand.nextInt(15);
+		int x = random.nextInt(16);
+		int y = random.nextInt(128);
+		int z = random.nextInt(16);
 		while (!suitable(y)) {
-			y = rand.nextInt(127);
+			y = random.nextInt(128);
 		}
 		// Only populates if the "target" location is air & there is netherrack
 		// above it. (and only checked in Y 117-127 for efficiency, might be
 		// changed later)
-		if (source.getBlock(x, y, z).getType() != Material.AIR) {
+		if (source.getBlock(x, y, z).getTypeId() != AIR) {
 			return;
 		}
-		if (source.getBlock(x, y, z).getRelative(BlockFace.UP).getType() != Material.NETHERRACK) {
+		if (source.getBlock(x, y, z).getRelative(BlockFace.UP).getTypeId() != NETHERRACK) {
 			return;
 		}
-		source.getBlock(x, y, z).setTypeId(89);
+		source.getBlock(x, y, z).setTypeId(GLOWSTONE);
+		x += source.getX() * 16;
+		z += source.getZ() * 16;
 		for (int l = 0; l < 1500; l++) {
-			int i1 = (x + random.nextInt(8)) - random.nextInt(8);
-			int j1 = y - random.nextInt(12);
-			int k1 = (z + random.nextInt(8)) - random.nextInt(8);
-			if (world.getBlockAt(i1, j1, k1).getTypeId() != 0) {
+			int currentX = (x + random.nextInt(8)) - random.nextInt(8);
+			int currentY = y - random.nextInt(12);
+			int currentZ = (z + random.nextInt(8)) - random.nextInt(8);
+			if (world.getBlockAt(currentX, currentY, currentZ).getTypeId() != 0) {
 				continue;
 			}
-			int l1 = 0;
-			for (int i2 = 0; i2 < 6; i2++) {
-				int j2 = 0;
-				if (i2 == 0) {
-					j2 = source.getBlock(i1 - 1, j1, k1).getTypeId();
+			int count = 0;
+			for (int face = 0; face < 6; face++) {
+				int type = 0;
+				if (face == 0) {
+					type = world.getBlockTypeIdAt(currentX - 1, currentY, currentZ);
 				}
-				if (i2 == 1) {
-					j2 = source.getBlock(i1 + 1, j1, k1).getTypeId();
+				if (face == 1) {
+					type = world.getBlockTypeIdAt(currentX + 1, currentY, currentZ);
 				}
-				if (i2 == 2) {
-					j2 = source.getBlock(i1, j1 - 1, k1).getTypeId();
+				if (face == 2) {
+					type = world.getBlockTypeIdAt(currentX, currentY - 1, currentZ);
 				}
-				if (i2 == 3) {
-					j2 = source.getBlock(i1, j1 + 1, k1).getTypeId();
+				if (face == 3) {
+					type = world.getBlockTypeIdAt(currentX, currentY + 1, currentZ);
 				}
-				if (i2 == 4) {
-					j2 = source.getBlock(i1, j1, k1 - 1).getTypeId();
+				if (face == 4) {
+					type = world.getBlockTypeIdAt(currentX, currentY, currentZ - 1);
 				}
-				if (i2 == 5) {
-					j2 = source.getBlock(i1, j1, k1 + 1).getTypeId();
+				if (face == 5) {
+					type = world.getBlockTypeIdAt(currentX, currentY, currentZ + 1);
 				}
-				if (j2 == 87) {
-					l1++;
+				if (type == GLOWSTONE) {
+					count++;
 				}
 			}
 
-			if (l1 == 1) {
-				source.getBlock(i1, j1, k1).setTypeId(87);
+			if (count == 1) {
+				world.getBlockAt(currentX, currentY, currentZ).setTypeId(GLOWSTONE);
 			}
 		}
 		return;
@@ -96,13 +91,13 @@ public class NotchGlowstonePopulator extends BananaBlockPopulator {
 	 * <br />
 	 * If the Y-coordinate is 52-72, it is OK. <br />
 	 * If the Y-coordinate is 114-127, it is OK.
-	 * 
+	 *
 	 * @param y
 	 *            Y-coordinate
 	 * @return true if the given Y-coordinate is suitable for glowstone
 	 *         generation
 	 */
-	public static boolean suitable(int y) {
+	static boolean suitable(int y) {
 		if (y > 113 && y < 128) {
 			return true;
 		}
@@ -111,5 +106,4 @@ public class NotchGlowstonePopulator extends BananaBlockPopulator {
 		}
 		return false;
 	}
-
 }
--- src/net/llamaslayers/minecraft/banana/gen/populators/NotchGlowstonePopulator2.java
+++ src/net/llamaslayers/minecraft/banana/gen/populators/NotchGlowstonePopulator2.java
@@ -6,7 +6,6 @@ import java.util.Random;
 
 // Bukkit Imports
 import org.bukkit.Chunk;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.BlockFace;
 
@@ -15,77 +14,73 @@ import net.llamaslayers.minecraft.banana.gen.BananaBlockPopulator;
 
 /**
  * Edited for BananaGen.
- * 
+ *
  * @author Markus 'Notch' Persson
  * @author iffa
  */
 public class NotchGlowstonePopulator2 extends BananaBlockPopulator {
-	// Variables
-	private Random rand = new Random();
-	private int x = rand.nextInt(15);
-	private int y = rand.nextInt(127);
-	private int z = rand.nextInt(15);
-
 	/**
 	 * Populates a world with glowstone (2). Easily configurable (but results in
 	 * more rare glowstone) by modifying NotchGlowstonePopulator's
 	 * suitable()-method.
-	 * 
+	 *
 	 * @author Markus 'Notch' Persson
 	 * @author iffa
 	 */
 	@Override
 	public void populate(World world, Random random, Chunk source) {
-		x = rand.nextInt(15);
-		y = rand.nextInt(127);
-		z = rand.nextInt(15);
+		int x = random.nextInt(15);
+		int y = random.nextInt(127);
+		int z = random.nextInt(15);
 		while (!NotchGlowstonePopulator.suitable(y)) {
-			y = rand.nextInt(127);
+			y = random.nextInt(127);
 		}
 		// Only populates if the "target" location is air & there is netherrack
 		// above it. (and only checked in Y 117-127 for efficiency, might be
 		// changed later)
-		if (source.getBlock(x, y, z).getType() != Material.AIR) {
+		if (source.getBlock(x, y, z).getTypeId() != AIR) {
 			return;
 		}
-		if (source.getBlock(x, y, z).getRelative(BlockFace.UP).getType() != Material.NETHERRACK) {
+		if (source.getBlock(x, y, z).getRelative(BlockFace.UP).getTypeId() != NETHERRACK) {
 			return;
 		}
-		source.getBlock(x, y, z).setTypeId(89);
+		source.getBlock(x, y, z).setTypeId(GLOWSTONE);
+		x += source.getX() * 16;
+		z += source.getZ() * 16;
 		for (int l = 0; l < 1500; l++) {
-			int i1 = (x + random.nextInt(8)) - random.nextInt(8);
-			int j1 = y - random.nextInt(12);
-			int k1 = (z + random.nextInt(8)) - random.nextInt(8);
-			if (source.getBlock(i1, j1, k1).getType() != Material.AIR) {
+			int currentX = (x + random.nextInt(8)) - random.nextInt(8);
+			int currentY = y - random.nextInt(12);
+			int currentZ = (z + random.nextInt(8)) - random.nextInt(8);
+			if (source.getBlock(currentX, currentY, currentZ).getTypeId() != AIR) {
 				continue;
 			}
-			int l1 = 0;
-			for (int i2 = 0; i2 < 6; i2++) {
-				int j2 = 0;
-				if (i2 == 0) {
-					j2 = source.getBlock(i1 - 1, j1, k1).getTypeId();
+			int count = 0;
+			for (int face = 0; face < 6; face++) {
+				int type = 0;
+				if (face == 0) {
+					type = world.getBlockTypeIdAt(currentX - 1, currentY, currentZ);
 				}
-				if (i2 == 1) {
-					j2 = source.getBlock(i1 + 1, j1, k1).getTypeId();
+				if (face == 1) {
+					type = world.getBlockTypeIdAt(currentX + 1, currentY, currentZ);
 				}
-				if (i2 == 2) {
-					j2 = source.getBlock(i1, j1 - 1, k1).getTypeId();
+				if (face == 2) {
+					type = world.getBlockTypeIdAt(currentX, currentY - 1, currentZ);
 				}
-				if (i2 == 3) {
-					j2 = source.getBlock(i1, j1 + 1, k1).getTypeId();
+				if (face == 3) {
+					type = world.getBlockTypeIdAt(currentX, currentY + 1, currentZ);
 				}
-				if (i2 == 4) {
-					j2 = source.getBlock(i1, j1, k1 - 1).getTypeId();
+				if (face == 4) {
+					type = world.getBlockTypeIdAt(currentX, currentY, currentZ - 1);
 				}
-				if (i2 == 5) {
-					j2 = source.getBlock(i1, j1, k1 + 1).getTypeId();
+				if (face == 5) {
+					type = world.getBlockTypeIdAt(currentX, currentY, currentZ + 1);
 				}
-				if (j2 == 89) {
-					l1++;
+				if (type == GLOWSTONE) {
+					count++;
 				}
 			}
-			if (l1 == 1) {
-				source.getBlock(i1, j1, k1).setTypeId(89);
+			if (count == 1) {
+				world.getBlockAt(currentX, currentY, currentZ).setTypeId(GLOWSTONE);
 			}
 		}
 		return;
